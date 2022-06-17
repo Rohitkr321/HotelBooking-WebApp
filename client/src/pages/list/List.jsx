7ba6c965ac44ca6e29baa35aa208df8d6b17896f
kritebh <kritebhlaganbibhakar@gmail.com>
2022-06-17 10:29:48 +0530

    homepage done

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
+import SearchItem from "../../components/searchItem/SearchItem"
+
+import "./list.css"
 
 function List() {
   return (
